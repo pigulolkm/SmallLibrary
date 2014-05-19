072957e44ad5c3b6ea214cc27be847924afb0ee1
pigulolkm <pigulolkm@gmail.com>
2014-05-20 01:06:45 +0800

    Finish multiple option search

--- src/com/example/smalllibrary/SearchBooksActivity.java
+++ src/com/example/smalllibrary/SearchBooksActivity.java
@@ -2,7 +2,6 @@ package com.example.smalllibrary;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -20,6 +19,7 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -34,6 +34,10 @@ public class SearchBooksActivity extends Activity {
 
 	private EditText editTextSearchKey;
 	private Spinner spinnerSeachOption;
+	private EditText editTextSearchKey2;
+	private Spinner spinnerSeachOption2;
+	private EditText editTextSearchKey3;
+	private Spinner spinnerSeachOption3;
 	
 	private String[] key;
     private ArrayAdapter<String> searchOptionAdapter;
@@ -51,6 +55,8 @@ public class SearchBooksActivity extends Activity {
         setListener();
         
         spinnerSeachOption.setAdapter(searchOptionAdapter);
+        spinnerSeachOption2.setAdapter(searchOptionAdapter);
+        spinnerSeachOption3.setAdapter(searchOptionAdapter);
 	}
 	
 	private void init() {
@@ -61,7 +67,11 @@ public class SearchBooksActivity extends Activity {
 	
 	private void findViews(){
 		editTextSearchKey = (EditText)findViewById(R.id.editTextSearchKey);
+		editTextSearchKey2 = (EditText)findViewById(R.id.editTextSearchKey2);
+		editTextSearchKey3 = (EditText)findViewById(R.id.editTextSearchKey3);
 		spinnerSeachOption = (Spinner)findViewById(R.id.spinnerSeachOption);
+		spinnerSeachOption2 = (Spinner)findViewById(R.id.spinnerSeachOption2);
+		spinnerSeachOption3 = (Spinner)findViewById(R.id.spinnerSeachOption3);
 	}
 	
 	private void setListener() {
@@ -89,6 +99,54 @@ public class SearchBooksActivity extends Activity {
 			@Override
 			public void onNothingSelected(AdapterView<?> arg0) {}
 	    });
+		
+		spinnerSeachOption2.setOnItemSelectedListener(new OnItemSelectedListener(){
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long arg3) {
+				
+				///////////////////////////////////////////
+				// Handle Case: Scan code, call scanner // 
+				/////////////////////////////////////////
+				if(arg0.getSelectedItem().toString().equals("Scan Code"))
+				{
+					if(isCameraAvailable())
+					{
+						Intent intent = new Intent();
+						intent.setClass(SearchBooksActivity.this, CameraTestActivity.class);
+						startActivityForResult(intent, Generic.scan_REQUEST+1);
+					}
+				}
+				
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {}
+	    });
+		
+		spinnerSeachOption3.setOnItemSelectedListener(new OnItemSelectedListener(){
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long arg3) {
+				
+				///////////////////////////////////////////
+				// Handle Case: Scan code, call scanner // 
+				/////////////////////////////////////////
+				if(arg0.getSelectedItem().toString().equals("Scan Code"))
+				{
+					if(isCameraAvailable())
+					{
+						Intent intent = new Intent();
+						intent.setClass(SearchBooksActivity.this, CameraTestActivity.class);
+						startActivityForResult(intent, Generic.scan_REQUEST+2);
+					}
+				}
+				
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {}
+	    });
 	}
 	
 	@Override
@@ -98,6 +156,18 @@ public class SearchBooksActivity extends Activity {
 				if(resultCode == RESULT_OK)
 				{
 					editTextSearchKey.setText(data.getStringExtra("SCAN_RESULT"));
+                } 
+                break;
+			case Generic.scan_REQUEST+1:
+				if(resultCode == RESULT_OK)
+				{
+					editTextSearchKey2.setText(data.getStringExtra("SCAN_RESULT"));
+                } 
+                break;
+			case Generic.scan_REQUEST+2:
+				if(resultCode == RESULT_OK)
+				{
+					editTextSearchKey3.setText(data.getStringExtra("SCAN_RESULT"));
                 } 
                 break;
 		}
@@ -114,18 +184,28 @@ public class SearchBooksActivity extends Activity {
 		else
 		{
 			String searchKey = editTextSearchKey.getText().toString();
+			String searchKey2 = editTextSearchKey2.getText().toString();
+			String searchKey3 = editTextSearchKey3.getText().toString();
 			String searchOption = spinnerSeachOption.getSelectedItem().toString();
+			String searchOption2 = spinnerSeachOption2.getSelectedItem().toString();
+			String searchOption3 = spinnerSeachOption3.getSelectedItem().toString();
 			// Remove the space of Scan Code
-			if(searchOption.equals("Scan Code"))
+			if(searchOption.equals("Scan Code") || searchOption2.equals("Scan Code") || searchOption3.equals("Scan Code"))
 			{
 				searchOption = "ScanCode";
+				searchOption2 = "ScanCode";
+				searchOption3 = "ScanCode";
 			}
 			String url = Generic.serverurl + "Book/PostGetBookByKey";
 			JSONObject json = new JSONObject();
 			try 
 			{
 				json.put( "searchKey" , searchKey);
+				json.put( "searchKey2" , searchKey2);
+				json.put( "searchKey3" , searchKey3);
 				json.put( "searchOption" , searchOption);
+				json.put( "searchOption2" , searchOption2);
+				json.put( "searchOption3" , searchOption3);
 				
 			} 
 			catch (JSONException e) 
@@ -145,6 +225,8 @@ public class SearchBooksActivity extends Activity {
 	///////////////////////
 	public void reset(View v) {
 		editTextSearchKey.setText("");
+		editTextSearchKey2.setText("");
+		editTextSearchKey3.setText("");
 	}
 	
 	private class GetSearchBooksOperation extends AsyncTask<String, Void, String>{
@@ -195,6 +277,7 @@ public class SearchBooksActivity extends Activity {
 		@Override
 		protected void onPostExecute(String result)
 		{
+			Log.d("SearchBooksActivity", result+"");
 			Dialog.dismiss();
 			Intent intent = new Intent(SearchBooksActivity.this, ShowSearchBooksResultActivity.class);
 			intent.putExtra("SearchBooksResult", result);
@@ -210,18 +293,23 @@ public class SearchBooksActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.search_books, menu);
+		//getMenuInflater().inflate(R.menu.search_books, menu);
 		return true;
 	}
 	
 	@Override
     public boolean onOptionsItemSelected(MenuItem menuItem)
     {   
-		Intent intent = new Intent();
-		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-		intent.setClass(SearchBooksActivity.this,MainActivity.class);
-        startActivity(intent); 
-        return true;
+		switch(menuItem.getItemId())
+		{
+	    	case android.R.id.home:
+			Intent intent = new Intent();
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+			intent.setClass(SearchBooksActivity.this,MainActivity.class);
+	        startActivity(intent); 
+	        return true;
+		}
+	    return false;
     }
 	
 	private boolean checkNetworkState()
@@ -245,5 +333,4 @@ public class SearchBooksActivity extends Activity {
         }
 		return false;
 	}
-
 }
